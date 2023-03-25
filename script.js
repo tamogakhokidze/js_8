d826454f41c7b415afecc1a07bb851aa5d4d92d1
Tamar Gakhokidze <63312406+tamogakhokidze@users.noreply.github.com>
2023-03-25 18:10:07 +0400

    changes

--- script.js
+++ script.js
@@ -1,71 +1,158 @@
-// დღევანდელ შეხვედრაზე,
-// https://reqres.in/api/users?page=2
-// მოცემული ლინკიდან გვაქვს გაკეთებული load more & load next and load previuos მაგალითები;
+// "use strict";
 
-//task 1
-//https://reqres.in/api/unknown წამოიღეთ fetch-ის საშუალებით name + color;
+//--------------------------fetch-------------------------
 
-// function getUser(){
-//     fetch('https://reqres.in/api/unknown')
-//     method: "GET"
-// };
+// task 1 წამოიღეთ fetch-ის საშუალებით name + color;
 
-// .then(function (mosuliinfo){
-//     if (mosuliinfo.status !== 200){
-//         throw mosuliinfo.status;
+// function getUsers{
+
+// fetch("https://reqres.in/api/unknown", {
+//   method: "GET",
+// })
+//   .then(function (mosuliInfo) {
+//     if (mosuliInfo.status !== 200) {
+//       throw mosuliInfo.status;
 //     }
-//     return mosuliinfo.json();
-// })
-// .then(function(mosuliinfoJs){
-//     const fragment = document.createDocumentFragment();
-// mosuliinfoJs.data.forEach(element => {
-//     let li = document.createElement('li');
-// li.textContent = `${element.name}, ${element.color}`;
-// fragment.appendChild(li);
+//     return mosuliInfo.json();
+//   })
 
-// });
-// })
-// .catch (function(error){
+//   .then(function (mosuliInfoJs) {
+//     console.log(mosuliInfoJs);
+
+//     mosuliInfoJs.data.forEach((element) => {
+//       let li = document.createElement("li");
+//       li.textContent = `${element.name} ${element.color}`;
+
+//       document.getElementById("users-info").appendChild(li);
+//       document.getElementById("ajax-block").appendChild(li);
+//     });
+//   })
+//   .catch(function (error) {
 //     console.log(error);
-//     if(error == 404){
-//         let p = document.createElement('p');
-//         p.textContent = "Page not Found";
-//         document.getElementById("ajax-block").appendChild(p);
-// document.getElementById("users-info").appendChild(fragment);
-//     }
+//   });
+
+//------------------------xmlhttp request-------------------
+
+//task 2  https://reqres.in/api/users?page=2  წამოიღეთ xml http requistის საშუალებით სახელი გვარი + სურათი;
+
+// let currentPage = 1;
+// let totalPagesServer;
+
+// function getUsers(page) {
+//   let request = new XMLHttpRequest();
+//   request.addEventListener("load", function render() {
+//     let response = this.responseText;
+//     let mosuliInfoJs = JSON.parse(response);
+
+//     let fragment = document.createDocumentFragment();
+
+//     mosuliInfoJs.data.forEach((element) => {
+//       let li = document.createElement("li");
+//       li.classList.add("li-wrapper");
+
+//       let pElement = document.createElement("p");
+//       pElement.textContent = `${element.first_name} ${element.last_name}`;
+
+//       let imgElement = document.createElement("img");
+
+//       // imgElement.setAttribute("src", `${element.avatar}`);
+
+//       imgElement.src = `${element.avatar}`;
+//       li.appendChild(pElement);
+//       li.appendChild(imgElement);
+//       fragment.appendChild(li);
+//     });
+
+//     document.getElementById("users-info").innerHTML = " ";
+//     document.getElementById("users-info").appendChild(fragment);
+
+//     totalPagesServer = mosuliInfoJs.total_pages;
+//   });
+//   request.open("GEt", 'https://reqres.in/api/users?page=" + page');
+//   request.send();
+// }
+
+// document.getElementById("prev").addEventListener("click", function () {
+//   if (currentPage == 1) {
+//     return;
+//   }
+//   currentPage -= 1;
+//   getUsers(currentPage);
 // });
 
-// getUser(mosuliinfo);
+// document.getElementById("next").addEventListener("click", function () {
+//   if (currentPage === totalPagesServer) {
+//     return;
+//   }
+//   currentPage += 1;
+//   getUsers(currentPage);
+// });
 
-// დავალება 2:
-// https://reqres.in/api/users?page=2 წამოიღეთ xml http requistის საშუალებით სახელი გვარი + სურათი;
+// getUsers(currentPage);
 
-//task 2
+// //   task 3 ლექციაზე დაწერილ კოდს load next & load prev users დაუმატეთ ვალიდაცია
 
-function getUser() {
-    let response = this.responseText;
-  console.log(this.responseText);
- 
-  let responseData = JSON.parse(response);
-  console.log(responseData);
+let currentPage = 1;
+let totalPagesServer;
+
+function getUsers(page) {
+  fetch("https://reqres.in/api/users?page=" + page, {
+    method: "GET",
+  })
+    .then(function (mosuliInfo) {
+      if (mosuliInfo.status !== 200) {
+        throw mosuliInfo.status;
+      }
+      return mosuliInfo.json();
+    })
+    .then(function (mosuliInfoJs) {
+      let fragment = document.createDocumentFragment();
+
+      mosuliInfoJs.data.forEach((element) => {
+        let li = document.createElement("li");
+        li.classList.add("li-wrapper");
+        let pElement = document.createElement("p");
+        pElement.textContent = `${element.first_name} ${element.last_name}`;
+
+        let imgElement = document.createElement("img");
+
+        // imgElement.setAttribute("src", `${element.avatar}`);
+
+        imgElement.src = `${element.avatar}`;
+        li.appendChild(pElement);
+        li.appendChild(imgElement);
+        fragment.appendChild(li);
+      });
+      document.getElementById("users-info").innerHTML = " ";
+      document.getElementById("users-info").appendChild(fragment);
+
+      totalPagesServer = mosuliInfoJs.total_pages;
+    })
+
+    .catch(function (error) {
+      console.log(error);
+      if (error == 404) {
+        let p = document.createElement("p");
+        p.textContent = "Page not found";
+        document.getElementById("ajax-block").appendChild(p);
+      }
+    });
 }
 
-let request = new XMLHttpRequest();
-
-request.addEventListener("load", getUser);
-request.open("GET", "https://reqres.in/api/users?page=2");
-request.send();
-
-getUser();
-
-responseData.data.forEach(element => {
-    let li = document.createElement('li');
-li.textContent = `${element.first_name}, ${element.last_name} , ${element.avatar}`;
-fragment.appendChild(li);
-document.getElementById("users-info").appendChild(fragment);
+document.getElementById("prev").addEventListener("click", function () {
+  if (currentPage == 1) {
+    return;
+  }
+  currentPage -= 1;
+  getUsers(currentPage);
 });
 
-// დავალება 3:
-// ლექციაზწ დაწერილ კოდს load next & load prev users დაუმატეთ ვალიდაცია
+document.getElementById("next").addEventListener("click", function () {
+  if (currentPage === totalPagesServer) {
+    return;
+  }
+  currentPage += 1;
+  getUsers(currentPage);
+});
 
-// წარმატებები
+getUsers(currentPage);
